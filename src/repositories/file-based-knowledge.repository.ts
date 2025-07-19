3fa1f846b14e840f30248e6739f4447421ab2d17
Ryota Kasahara <80407935+hojicha0915@users.noreply.github.com>
2025-07-19 17:28:03 +0900

    Update file-based-knowledge.repository.ts

--- src/repositories/file-based-knowledge.repository.ts
+++ src/repositories/file-based-knowledge.repository.ts
@@ -6,7 +6,17 @@ export const FileBasedKnowledgeRepository: KnowledgeRepository = {
   // @ts-ignore TODO: (学生向け) 実装する
   getAll: async () => {},
   // @ts-ignore TODO: (学生向け) 実装する
-  upsert: async (knowledge) => {},
+  upsert: async (knowledge) => {
+    const filepath = path.join(
+      __dirname,
+      `strage/knowledge-${knowledge.id}.json`
+    );
+    const content = `${knowledge}`;
+    await writeFile(filepath, content, "utf-8");
+  },
   // @ts-ignore TODO: (学生向け) 実装する
-  deleteById: async (id) => {},
+  deleteById: async (id) => {
+    const filepath = path.join(__dirname, `strage/knowledge-${id}.json`);
+    await unlink(filepath);
+  },
 };
